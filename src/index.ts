42180c9827fa21130a6d3e0c56f320da07098fff
James Buncle <jbuncle@hotmail.com>
2023-03-21 21:31:23 +0000

    Updated to use granular log levels (to suppress some logging)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 import { ConsoleLoggerFactory, LogLevel, Logger } from '@jbuncle/logging-js';
-Logger.setLoggerFactory(new ConsoleLoggerFactory(LogLevel.DEBUG));
+
+
+Logger.setLoggerFactory(new ConsoleLoggerFactory(LogLevel.DEBUG, {
+    '^@jbuncle/pigeon-proxy-server/ModSecurity$' : LogLevel.INFO,
+    '^@jbuncle/pigeon-proxy-server/ModSecurityLoader$' : LogLevel.INFO,
+    '^@jbuncle/pigeon-proxy-server/DockerProxyRouter$' : LogLevel.INFO,
+    '^@jbuncle/pigeon-proxy-server/DockerMonitor$' : LogLevel.INFO,
+    '^@jbuncle/pigeon-proxy-server/SNICallbackFactory$' : LogLevel.INFO,
+
+}));
 
 
 import { App } from './App';
